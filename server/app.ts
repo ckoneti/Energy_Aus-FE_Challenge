f542e9e1456813848624bc78e3d64ccf59fe6fa9
CK <chitrakoneti@gmail.com>
2024-11-28 11:34:30 +1100

    Delete server directory

--- server/app.ts
+++
@@ -1,8 +0,0 @@
-import { getFestivalDetails } from "./festival-details"
-export const getMusicFestivalDetails = async():Promise<any>=>{
-    return await getFestivalDetails();
-}
-
-getMusicFestivalDetails().then(festival =>
-   console.log(festival, 'festival')
-)
